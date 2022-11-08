d2d301643879f34963b9d19dd88ceff10083e99c
Jorsary <edikus29@gmail.com>
2022-11-08 21:42:17 +0300

    fix: firebase api

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyAHrujR7n8q6UPorLLQxx0v9SYwjns8_xI',
+  authDomain: 'task-manager-f1d8b.firebaseapp.com',
+  projectId: 'task-manager-f1d8b',
+  storageBucket: 'task-manager-f1d8b.appspot.com',
+  messagingSenderId: '706813468472',
+  appId: '706813468472:web:3d1ede580c13134c80abcd',
+  measurementId: 'G-5X7YEYD32W'
 };
 
 
